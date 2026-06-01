06769b1e2fd28ecd677a6bd99961a43cd08acda9
Nikodem Czachorowski <czachorowski123@gmail.com>
2026-06-02 00:21:57 +0200

    fixed unimplemented nop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use crate::files::read_program_file;
 use crate::{
     files::{print_mem, read_memory_file},
     simulation::Simulation,
 };
-use crate::files::read_program_file;
 
 mod files;
 mod instruction;
@@ -13,8 +13,11 @@ mod simulation;
 
 fn main() {
     let mut mem = read_memory_file("pipe2.dat".to_string());
-    print_mem(&mem);
-    let prog = read_program_file("pipe2.cod".to_string(), &mut mem);
-    let mut sim = Simulation::new(2000, 32);
-    sim.tick();
+    read_program_file("pipe2.cod".to_string(), &mut mem);
+    let mut sim = Simulation::new(mem, 32);
+    while sim.ip < 500 {
+        sim.tick();
+    }
+
+    print_mem(sim.get_memory());
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -1,5 +1,5 @@
-use crate::{instruction::Instruction, memory::Memory, register_bank::RegisterBank};
 use crate::jump_prediction::Jump_Prediction;
+use crate::{instruction::Instruction, memory::Memory, register_bank::RegisterBank};
 
 const PIPELINE_LENGTH: usize = 5;
 
@@ -8,14 +8,14 @@ pub struct Simulation {
     register_bank: RegisterBank,
     jump_prediction: Jump_Prediction,
     pipeline: [Instruction; PIPELINE_LENGTH],
-    ip: u32,
+    pub ip: u32,
     head: usize,
 }
 
 impl Simulation {
-    pub fn new(mem_size: u32, reg_count: u32) -> Self {
+    pub fn new(mem: Memory, reg_count: u32) -> Self {
         Self {
-            memory: Memory::new(mem_size),
+            memory: mem,
             jump_prediction: Jump_Prediction::new(),
             register_bank: RegisterBank::new(reg_count),
             pipeline: [Instruction::new(); PIPELINE_LENGTH],
@@ -28,12 +28,17 @@ impl Simulation {
         let former_ip = self.ip;
         self.pipeline[self.head].fetch(&mut self.ip, &self.memory, &mut self.jump_prediction);
         self.pipeline[(self.head + 1) % PIPELINE_LENGTH].decode(&self.register_bank);
-        self.pipeline[(self.head + 2) % PIPELINE_LENGTH].execute(&mut self.jump_prediction, &mut self.ip);
+        self.pipeline[(self.head + 2) % PIPELINE_LENGTH]
+            .execute(&mut self.jump_prediction, &mut self.ip);
         self.pipeline[(self.head + 3) % PIPELINE_LENGTH].memory(&mut self.memory);
         self.pipeline[(self.head + 4) % PIPELINE_LENGTH].write_back(&mut self.register_bank);
         if former_ip == self.ip {
             self.ip += 4
         }
         self.head = (self.head + 1) % PIPELINE_LENGTH;
+    }
+
+    pub fn get_memory(&self) -> &Memory {
+        &self.memory
     }
 }
